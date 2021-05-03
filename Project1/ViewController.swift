4a972ffce67657861d9a16275daa7821e97a80a9
mathieu-tulpinck <mathieu.tulpinck@student.ehb.be>
2021-05-03 15:03:17 +0200

    "first-commit"

---
+++ Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by mathieu on 03/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
